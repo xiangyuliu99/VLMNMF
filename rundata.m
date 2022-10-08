b2a1be9f860c6c8bc517dadedc5594b6204295bd
xiangyuliu99 <xiangyuliu99@gmail.com>
2022-10-08 11:14:09 +0800

    vlmnmf

--- rundata.m
+++
@@ -1,9 +0,0 @@
-x=3:1:61;
-plot(x,log(3:1:61),'bo-');
-set(gcf,'PaperSize',[16,11]);
-set(gca,'FontSize',16);
-set(gca,'FontWeight','bold');
-xlabel('Number of Iterations k');
-ylabel('Objective value')
-% legend('Objective value')
-saveas(gca,['F:\坚果云\我的坚果云\笔记/converage(handwritten).pdf']);
